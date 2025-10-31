b6dbeedd69f967237481510161d7a1ac411748bd
null <null>
2025-10-31 07:00:37 +0100

    avoid double diff() calls

--- src/custom.ts
+++ src/custom.ts
@@ -3,26 +3,26 @@ import { Component, ElementRef, computed, input, linkedSignal, model, output, si
 @Component({
   selector: 'app-card-chip-delete',
   template: `
-   <dialog #dialog closedby="none">
-      <form method="dialog">
-        <p #message>&nbsp;</p>
-        <button autofocus>Cancelar</button>
-        <button (click)="accepted()">Aceptar</button>
-      </form>
-    </dialog>
-
-    @for (entity of data(); track key()(entity)) {
-      @let text = chipText()(entity);
-      <button (click)="modalConfirm(entity)">
-        <span>{{text}}</span>
-        <span>&nbsp;&times;</span>
-      </button>
-    }
-   
+  <dialog #dialog closedby="none">
+  <form method="dialog">
+  <p #message>&nbsp;</p>
+  <button autofocus>Cancelar</button>
+  <button (click)="accepted()">Aceptar</button>
+  </form>
+  </dialog>
+  
+  @for (entity of data(); track key()(entity)) {
+    @let text = chipText()(entity);
+    <button (click)="modalConfirm(entity)">
+    <span>{{text}}</span>
+    <span>&nbsp;&times;</span>
+    </button>
+  }
+  
   `,
 })
 export class CardChipDelete<T> {
-
+  
   dialog = viewChild('dialog', { read: ElementRef<HTMLDialogElement> });
   message = viewChild('message', { read: ElementRef<HTMLParagraphElement> });
   data = model.required<Array<T>>();
@@ -31,22 +31,24 @@ export class CardChipDelete<T> {
   chipText = input.required<(entity: T) => string | number>();
   messageText = input.required<(entity: T) => string | number>();
   accept = output<T>();
-
+  
   accepted() {
-    const diff = this.key();
+    const keyFn = this.key();
+    const targetKey = keyFn(this.entityToRemove());
+    
     this.data.update(
-      (data) => 
-        {
-          return data.filter(
-            (entityToRemove)=>{
-              return diff(entityToRemove)!==diff(this.entityToRemove())
-            }
+      (data) => {
+        return data.filter(
+          (entityToRemove)=>{
+            return keyFn(entityToRemove)!== targetKey
+          }
           )
         }
-      )
-    this.accept.emit(this.entityToRemove());
+      );
+        
+      this.accept.emit(this.entityToRemove());
   }
-
+      
   modalConfirm(entity: T) {
     this.dialog()?.nativeElement.showModal();
     const span = this.message()?.nativeElement as HTMLParagraphElement;
@@ -54,3 +56,4 @@ export class CardChipDelete<T> {
     this.entityToRemove.set(entity);
   }
 }
+    
